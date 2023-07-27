5bf68bcad574fa0ce4519679c90a352505541cff
CyrusNg100 <69127518+CyrusNg100@users.noreply.github.com>
2023-07-26 20:39:15 -0400

    Add files via upload

---
+++ Sales Project/DIM_Calendar.sql
@@ -0,0 +1,23 @@
+-- Cleansed DimDate Table --
+SELECT [DateKey]
+      ,[FullDateAlternateKey] as Date
+    --,[DayNumberOfWeek]
+      ,[EnglishDayNameOfWeek] as Day
+    --,[SpanishDayNameOfWeek]
+    --,[FrenchDayNameOfWeek]
+    --,[DayNumberOfMonth]
+    --,[DayNumberOfYear]
+      ,[WeekNumberOfYear] as WeekNr
+      ,[EnglishMonthName] as Month
+	  ,left(EnglishMonthName,3) as MonthShort
+    --,[SpanishMonthName]
+    --,[FrenchMonthName]
+      ,[MonthNumberOfYear] as MonthNo
+      ,[CalendarQuarter] as Quarter
+      ,[CalendarYear] as Year
+    --,[CalendarSemester]
+    --,[FiscalQuarter]
+    --,[FiscalYear]
+    --,[FiscalSemester]
+  FROM [AdventureWorksDW2022].[dbo].[DimDate]
+  where CalendarYear >= 2021
